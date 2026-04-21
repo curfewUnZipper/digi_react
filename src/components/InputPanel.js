4a47e1b0e01c278323991b5f1c4b2619f98ceee9
Da Evil <123114180+curfewUnZipper@users.noreply.github.com>
2026-04-21 10:13:59 +0530

    updating uri

--- src/components/InputPanel.js
+++ src/components/InputPanel.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5000/predict";
+const API_URL = "https://digi-flask.vercel.app//predict";
 
 export default function InputPanel({ data, setData }) {
 
